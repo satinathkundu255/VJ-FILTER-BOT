20fed9a5b63fe5e2939103fabf62d97b2c893a8a
satinathkundu255 <satinathkundu7@gmail.com>
2026-03-05 14:20:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://i.ibb.co/jZhytSGj/7182526352-27918.jpg')).split()
+PICS = (environ.get('PICS', ' https://i.ibb.co/bgcL6LnX/7182526352-27973.jpg')).split()
 
 
 # Admins & Users
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
